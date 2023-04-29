4c4521f29aa17bbab9ac7763969032421548c493
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-29 18:40:11 +0200

    made github ribbon into component

--- app.js
+++ app.js
@@ -2,27 +2,7 @@
 import { html, Component, render } from './js/spux.js'
 import { getPath, getQueryStringValue, loadFile, saveFile } from './util.js'
 import './js/dior.js'
-
-function GithubRibbon({ repo }) {
-  return html`
-    <a
-      href="${repo}"
-      class="github-fork"
-      style="position: fixed; top: 0; right: 0; z-index: 1000;"
-    >
-      <img
-        decoding="async"
-        loading="lazy"
-        width="149"
-        height="149"
-        src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
-        class="attachment-full size-full"
-        alt="Fork me on GitHub"
-        data-recalc-dims="1"
-      />
-    </a>
-  `;
-}
+import GithubRibbon from './components/GithubRibbon.js'
 
 export class App extends Component {
   constructor() {
